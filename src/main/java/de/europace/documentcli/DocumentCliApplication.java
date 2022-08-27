6c7d561f09ab1c9da5aa2d9ec26371e432493448
21002329 <alaz.ozyar@gmail.com>
2022-08-27 11:53:12 +0200

    initial commit - Spring Initializr base

---
+++ src/main/java/de/europace/documentcli/DocumentCliApplication.java
@@ -0,0 +1,13 @@
+package de.europace.documentcli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocumentCliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocumentCliApplication.class, args);
+	}
+
+}
